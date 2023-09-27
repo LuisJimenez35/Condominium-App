c5b6d8e24a913d43734fc41c072b4af05a5f18ac
Luis Mora <82769778+LuisJimenez35@users.noreply.github.com>
2023-09-27 15:48:30 -0600

    Update

--- CondominiumProject/CondominiumProject/Controllers/HomeController.cs
+++ CondominiumProject/CondominiumProject/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using CondominiumProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Diagnostics;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace CondominiumProject.Controllers
 {
@@ -16,10 +18,50 @@ namespace CondominiumProject.Controllers
 		//Ventana de usuario Root
 		public IActionResult RootIndex()
 		{
-			return View();
-		}
-		//Ventana Guardia Seguridad
-		public IActionResult GuardIndex()
+            ViewBag.RootHasAccessToProjects = true;
+
+            if (!ViewBag.RootHasAccessToProjects)
+            {
+                ViewBag.Error = new ErrorHandler()
+                {
+                    Title = "Security validation",
+                    ErrorMessage = "User do not have access to this page",
+                    ActionMessage = "Go to login",
+                    Path = "/Login"
+                };
+
+                return View("ErrorHandler");
+            }
+
+            ViewBag.ProjectLists = GetHabitationalProject();
+
+            return View();
+        }
+
+        public List<HabitationalProjects> GetHabitationalProject()
+        {
+            List<HabitationalProjects> projectslist = new List<HabitationalProjects>();
+            DataTable ds = Database.DatabaseHelper.ExecuteQuery("spGetHabitationalProjects", null);
+
+            foreach (DataRow dr in ds.Rows)
+            {
+                projectslist.Add(new HabitationalProjects
+                {
+                    IdProject = Convert.ToInt32(dr["IdProject"]),
+                    Logo = dr["Logo"].ToString(),                    
+                    Code = dr["Code"].ToString(),
+                    Name = dr["Name"].ToString(),
+                    Adress = dr["Adress"].ToString(),
+                    OfficeTelephone = dr["OfficeTelephone"].ToString()
+                });
+            }
+            return projectslist;
+
+        }
+
+
+        //Ventana Guardia Seguridad
+        public IActionResult GuardIndex()
 		{
 			return View();
 		}
---
+++ CondominiumProject/CondominiumProject/Models/HabitationalProjects.cs
@@ -0,0 +1,12 @@
+﻿namespace CondominiumProject.Models
+{
+    public class HabitationalProjects
+    {
+        public int? IdProject { get; set; }
+        public string? Logo { get; set; }
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Adress { get; set; }
+        public string? OfficeTelephone { get; set; }
+    }
+}
